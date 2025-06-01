f03daba300c51db26ff2bad70f93134742804683
michaelolson01 <michaelolson01@gmail.com>
2025-06-01 11:50:10 -0500

    Added wifi interface to esp32 code

--- firmware/main/main.ino
+++ firmware/main/main.ino
@@ -2,29 +2,86 @@
 #include <Wire.h>
 #include <BH1750.h>
 #include <Adafruit_SHT31.h>
+#include <Ticker.h>
+#include <WiFi.h>
 
 BH1750 lightMeter;
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
+Ticker soilMoistureTicker;
+Ticker lightLevelTicker;
+Ticker wifiTicker;
+
+const int soilPin = 32;
+
+const char* ssid = "OlsonHome";
+const char* password = "Cr34myT4c0";
+bool already_connected = false;
+
+void initWifi() {
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(ssid, password);
+}
+
+void testWifiConnection() {
+	int status = WiFi.status();
+	if ((status == WL_CONNECTED) && (!already_connected)) {
+		Serial.print("Connected to ");
+		Serial.print(WiFi.localIP());
+		already_connected = true;
+	} else if (status == WL_CONNECT_FAILED) {
+		already_connected = false;
+		Serial.println("Unable to connect to WiFi.");
+	} else if (status == WL_DISCONNECTED) {
+		already_connected = false;
+		Serial.println("Disconnected from WiFi.");
+	} else if (status == WL_CONNECTION_LOST) {
+		already_connected = false;
+		Serial.println("Connection to WiFi Lost.");
+	} else if (status != WL_CONNECTED){
+		Serial.println("Still Connecting...");
+	}
+}
+
+void soilMoistureSensorAction() {
+	int soilMoistureValue = analogRead(soilPin);
+	Serial.print("Soil Moisture: "); Serial.print(soilMoistureValue); Serial.println(" units");
+}
+
+void lightLevelSensorAction() {
+	uint16_t lux = lightMeter.readLightLevel();
+	Serial.print("Light: "); Serial.print(lux); Serial.println(" lx");
+}
 
 void setup() {
-  Serial.begin(115200);
-  Wire.begin();
+	initWifi();
+    Serial.begin(9600);
+    // Initialize the Wire
+    Wire.begin();
+    if (lightMeter.begin()) {
+        Serial.println(F("BH1750 light sensor initialized"));
+    } else {
+        Serial.println(F("BH1750 Initialization failure!"));
+    }
 
-  lightMeter.begin();
-  if (!sht31.begin(0x44)) {
-    Serial.println("Couldn't find SHT31");
-    while (1) delay(1);
-  }
+    analogReadResolution(12);
+
+	soilMoistureTicker.attach(600.0, soilMoistureSensorAction);
+	lightLevelTicker.attach(60.0, lightLevelSensorAction);
+	wifiTicker.attach(5.0, testWifiConnection);
 }
 
 void loop() {
-  float temp = sht31.readTemperature();
-  float humidity = sht31.readHumidity();
-  uint16_t lux = lightMeter.readLightLevel();
 
-  Serial.print("Temp: "); Serial.print(temp); Serial.print(" C\t");
-  Serial.print("Humidity: "); Serial.print(humidity); Serial.print(" %\t");
-  Serial.print("Light: "); Serial.print(lux); Serial.println(" lx");
+  // float temp = sht31.readTemperature();
+  // float humidity = sht31.readHumidity();
+  /// uint16_t lux = lightMeter.readLightLevel();
+  // Lower == wetter
+  // int soilMoistureValue = analogRead(soilPin);
 
-  delay(2000);
+  // Serial.print("Temp: "); Serial.print(temp); Serial.print(" C\t");
+  // Serial.print("Humidity: "); Serial.print(humidity); Serial.print(" %\t");
+  // Serial.print("Light: "); Serial.print(lux); Serial.println(" lx");
+  // Serial.print("Soil Moisture: "); Serial.print(soilMoistureValue); Serial.println(" units");
+
+  // delay(2000);
 }
