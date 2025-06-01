2ca89f5f286d328a34e8f5cf2314432f7f8239f9
michaelolson01 <michaelolson01@gmail.com>
2025-06-01 11:51:05 -0500

    added secrets file to store ssid and password

---
+++ secrets.h
@@ -0,0 +1,7 @@
+#ifndef _secrets_
+#define _secrets_
+
+extern const char *WIFI_SSID;
+extern const char *WIFI_PASSWORD;
+
+#endif
---
+++ secrets_example.cpp
@@ -0,0 +1,7 @@
+// secrets.cpp to hold the values of the secrets
+
+#include "secrets.h"
+
+// Note - esp32 boards can only connect to 2.4MHz networks.
+const char *WIFI_SSID = "Your wifi SSID";
+const char *WIFI_PASSWORD = "Your wifi password";
