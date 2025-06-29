040230092f1c16313fc0c0bc570524afb741aadc
michaelolson01 <michaelolson01@gmail.com>
2025-06-29 12:17:04 -0500

    changed ESP_NOW_RECEIVER_MAC to HUB_MAC for clarity

--- src/secrets/secrets.h
+++ src/secrets/secrets.h
@@ -6,6 +6,6 @@ extern const char *WIFI_PASSWORD;
 extern const char *UDP_HOST;
 extern const int UDP_PORT;
 extern const char *OTA_PASSWORD;
-extern const uint8_t *ESP_NOW_RECEIVER_MAC;
+extern const uint8_t *HUB_MAC;
 
 #endif
--- src/secrets/secrets_example.cpp
+++ src/secrets/secrets_example.cpp
@@ -9,5 +9,5 @@ const char *UDP_HOST = "Host address";
 const int UDP_PORT = 0000; // Insert port here
 const char *OTA_PASSWORD = "An OTA Password"
 // The MAC address of the Receiver ESP32 module
-const uint8_t *ESP_NOW_RECEIVER_MAC = {0x99, 0x99, 0x99, 0x99, 0x99, 0x99};
+const uint8_t *HUB_MAC = {0x99, 0x99, 0x99, 0x99, 0x99, 0x99};
 
